f9cee4fbb8f5a68ec9b792c3450bffe28a99e766
TheResearchLab <aarontellis1996@outlook.com>
2024-03-23 18:16:22 -0400

    feat: add function to stage financial statements

--- src/python/fundamentals.py
+++ src/python/fundamentals.py
@@ -1,8 +1,9 @@
 
 import pandas as pd
-from db_connection import engine,session,api_key,text
+from db_connection import api_key,engine,sessionmaker,text,session
 import asyncio 
 import aiohttp
+import sys
 
 
 async def get_exchange_df() -> pd.DataFrame:
@@ -29,23 +30,37 @@ async def get_ticker_df(exchange_list: pd.Series) -> pd.DataFrame:
     return ticker_df.reset_index(drop=True)
 
 
-async def fetch_financials(session,code,exchange,type,freq):
+async def fetch_financials(session,code,exchange,ftype,freq):
     url = f"https://eodhd.com/api/fundamentals/{code}.{exchange}?api_token={api_key}&fmt=json"
     async with session.get(url) as response:
-        financials = await response.json()
-        financials_df = pd.DataFrame.from_dict(financials['Financials'][type][freq]).T.reset_index(drop=True)
-        financials_df['code'] = code
-        financials_df['exchange'] = exchange
+        try:
+            if response.status == 200:
+                financials = await response.json()
+                financials_df = pd.DataFrame.from_dict(financials['Financials'][ftype][freq]).T.reset_index(drop=True)
+                financials_df['code'] = code
+                financials_df['exchange'] = exchange
+                financials_df['type'] = ftype
+                financials_df['freq'] = freq
+            else:
+                raise Exception(f"HTTP Error: status:{response.status} code: {code} exchange: {exchange}")
+
+        except Exception as e:
+            print(f'Error has occurred: {e}')
+            sys.exit(0)
     return financials_df
         
 
-async def get_financial_df(code_list:pd.Series,exchange_list:pd.Series,type,freq) -> pd.DataFrame:
-    financial_df = pd.DataFrame()
+async def get_financials(code_list:pd.Series,exchange_list:pd.Series) -> list:
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch_financials(session,code,exchange,type,freq) for code, exchange in zip(code_list,exchange_list)]
-        financial_dfs = await asyncio.gather(*tasks)
-        financial_df = pd.concat(financial_dfs)
-    return financial_df.reset_index(drop=True)
+        tasks = []
+        for code, exchange in zip(code_list,exchange_list):
+            for freq in ['quarterly','yearly']:
+                for ftype in ['Balance_Sheet','Income_Statement','Cash_Flow']:
+                    tasks.append(fetch_financials(session, code, exchange,ftype,freq))
+            
+        all_data = await asyncio.gather(*tasks)
+
+        return all_data
 
 
 
@@ -65,9 +80,10 @@ async def main():
     # load common stock balance sheet, income statement, and cashflow statements
     cs_ticker_cd = ticker_df[ticker_df['Type'] == 'Common Stock'].Code
     cs_exchange_cd = ticker_df[ticker_df['Type'] == 'Common Stock'].exchange_cd
-    cs_balance_sheet_df = await get_financial_df(cs_ticker_cd,cs_exchange_cd,'Balance_Sheet','quarterly')
 
-
+    
+    financial_data =  await get_financials(cs_ticker_cd.iloc[:1],cs_exchange_cd.iloc[:1])
+    
     # load exchange
     # exchange_df.to_sql(name="stg_exchange",con=engine,if_exists="replace")
 
@@ -77,9 +93,31 @@ async def main():
     #   chunk.to_sql("stg_ticker", con=engine, if_exists='append', index=False)
 
     # load common stock balance sheet
-    for i in range(0,len(cs_balance_sheet_df),chunksize):
-        chunk = cs_balance_sheet_df[i:i+chunksize]
-        chunk.to_sql("stg_balance_sheet_q",con=engine,if_exists='append',index=False)
+    # for i in range(0,len(cs_balance_sheet_df),chunksize):
+    #     chunk = cs_balance_sheet_df[i:i+chunksize]
+    #     chunk.to_sql("stg_balance_sheet_q",con=engine,if_exists='append',index=False)
+
+    for stmt in financial_data:
+            ftype = stmt['type'].iloc[0]
+            freq = stmt['freq'].iloc[0]
+
+            if ftype == 'Income_Statement' and freq == 'yearly':
+                stmt.to_sql("stg_income_statement_yr",con=engine,if_exists='append',index=False)
+
+            if ftype == 'Income_Statement' and freq == 'quarterly':
+                stmt.to_sql("stg_income_statement_q",con=engine,if_exists='append',index=False)
+
+            if ftype == 'Balance_Sheet' and freq == 'yearly':
+                stmt.to_sql("stg_balance_sheet_yr",con=engine,if_exists='append',index=False)
+
+            if ftype == 'Balance_Sheet' and freq == 'quarterly':
+                stmt.to_sql("stg_balance_sheet_q",con=engine,if_exists='append',index=False)
+
+            if ftype == 'Cash_Flow' and freq == 'yearly':
+                stmt.to_sql("stg_cashflow_yr",con=engine,if_exists='append',index=False)
+
+            if ftype == 'Cash_Flow' and freq == 'quarterly':
+                stmt.to_sql("stg_cashflow_q",con=engine,if_exists='append',index=False)
 
 
     
